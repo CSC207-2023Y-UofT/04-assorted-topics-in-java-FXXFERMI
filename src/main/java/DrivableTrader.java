9895fee26d7ce2db6917a795c6627a5a07813ed7
FXXFERMI <fermi.fei@mail.utoronto.ca>
2023-06-10 21:58:43 -0400

    Done all the TODO

--- src/main/java/DrivableTrader.java
+++ src/main/java/DrivableTrader.java
@@ -11,3 +11,24 @@
  */
 
 import java.util.List;
+public class DrivableTrader extends Trader<Drivable> {
+    public DrivableTrader(List<Drivable> inventory, List<Drivable> wishlist, int money) {
+        super(inventory, wishlist, money);
+    }
+
+
+    public DrivableTrader(int money) {
+        super(money);
+    }
+
+    @Override
+    public int getSellingPrice(Drivable item) {
+        if (item instanceof Tradable) {
+            int price = ((Tradable) item).getPrice();
+            int maxSpeed = item.getMaxSpeed();
+            return price + maxSpeed;
+        } else {
+            return Tradable.MISSING_PRICE;
+        }
+    }
+}
